3ba6ba6c44ab384cd6620cf3b6ee81bff9268036
Sanjay Madnani <sanjay.madnani@outlook.com>
2014-06-20 19:05:00 +0530

    Basic Structure of class. Implementation pending.

---
+++ propertiesBundle/src/main/java/com/sanjay/util/PropertyUtil.java
@@ -0,0 +1,34 @@
+/* Copyright (C) 2014, 2015 Sanjay Madnani
+ * 
+ * This file is free to use: you can redistribute it and/or modify it under the terms of the GPL General Public License
+ * V2 as published by the Free Software Foundation, subject to the following conditions:
+ * 
+ * The above copyright notice should never be changed and should always included wherever this file is used.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ * See the GNU General Public License V2 for more details. */
+package com.sanjay.util;
+
+import java.util.Properties;
+
+public final class PropertyUtil {
+
+    /**
+     * Private Construct to restrict object creation.
+     */
+    private PropertyUtil() {
+    }
+
+    
+    private static Properties loadProperty(String property) {
+        return null;
+    }
+
+    public static String getValue(String key, String property) {
+        return null;
+    }
+
+    public static String getFormatedStringValue(String key, String property, Object... arguments) {
+        return null;
+    }
+}
