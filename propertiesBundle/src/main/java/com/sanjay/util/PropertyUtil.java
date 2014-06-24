9c8a849e5d67e23c317957307e7e5126813e4e77
Sanjay Madnani <sanjay.madnani@outlook.com>
2014-06-24 20:47:35 +0530

    Modified by adding the basic structure. Signed-off-by: Sanjay Madnani <sanjay.madnani@outlook.com>

--- propertiesBundle/src/main/java/com/sanjay/util/PropertyUtil.java
+++ propertiesBundle/src/main/java/com/sanjay/util/PropertyUtil.java
@@ -9,8 +9,19 @@
  * See the GNU General Public License V2 for more details. */
 package com.sanjay.util;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.text.MessageFormat;
 import java.util.Properties;
 
+/**
+ * Utility for Loading & Retrieving Properties values.
+ * 
+ * @author SANJAY
+ * @see Properties
+ * @see MessageFormat
+ */
 public final class PropertyUtil {
 
     /**
@@ -19,16 +30,44 @@ public final class PropertyUtil {
     private PropertyUtil() {
     }
 
-    
-    private static Properties loadProperty(String property) {
-        return null;
+    /**
+     * Loads a property by propertyName.
+     * 
+     * @param propertyName String property name.
+     * @return Properties loaded using argument.
+     */
+    private static Properties loadProperty(String propertyName) {
+        try {
+            Properties properties = new Properties();
+            Reader reader = new FileReader(propertyName);
+            properties.load(reader);
+            return properties;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public static String getValue(String key, String property) {
-        return null;
+    /**
+     * Returns Object value corresponding to properyName.
+     * @param key the key whose associated value is to be returned.
+     * @param properyName
+     * @return
+     */
+    public static Object getValue(String key, String properyName) {
+        Properties properties = loadProperty(properyName);
+        return properties.get(key);
     }
 
+    /**
+     * 
+     * @param key the key whose associated value is to be returned.
+     * @param property
+     * @param arguments
+     * @return
+     */
     public static String getFormatedStringValue(String key, String property, Object... arguments) {
-        return null;
+        Properties properties = loadProperty(property);
+        return MessageFormat.format(properties.getProperty(key), arguments);
     }
 }
