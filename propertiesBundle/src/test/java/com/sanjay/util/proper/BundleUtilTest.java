72ca426c894e13c9a7219c159dc21a2166ec2e38
Sanjay Madnani <sanjay.madnani@outlook.com>
2014-06-24 17:43:37 +0530

    Initial commit-need to be implemented.

---
+++ propertiesBundle/src/test/java/com/sanjay/util/proper/BundleUtilTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.sanjay.util.proper;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sanjay.madnani
+ *
+ */
+public class BundleUtilTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    /**
+     * Test method for {@link com.sanjay.util.proper.BundleUtil#getStringMessage(java.lang.String)}.
+     */
+    @Test
+    public final void testGetStringMessage() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link com.sanjay.util.proper.BundleUtil#getFormatedMessage(java.lang.String, java.lang.Object[])}.
+     */
+    @Test
+    public final void testGetFormatedMessage() {
+        fail("Not yet implemented");
+    }
+
+}
