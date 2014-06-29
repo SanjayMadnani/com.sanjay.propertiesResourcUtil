12847cce6e382c72bb5d0c78510244efac4d4c42
Sanjay Madnani <sanjay.madnani@outlook.com>
2014-06-29 15:23:19 +0530

    Constants & properties added for code teting and for demonstration of common requirements.
    Signed-off-by: Sanjay Madnani <sanjay.madnani@outlook.com>

---
+++ propertiesBundle/src/main/java/com/sanjay/constants/MessagesConstants.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sanjay.constants;
+
+/**
+ * @author SANJAY
+ * 
+ */
+public final class MessagesConstants {
+
+    /**
+     * Private Construct to restrict object creation.
+     */
+    private MessagesConstants() {
+
+    }
+
+    public static final String EMPTY_MAIL_ID = "ERROR.EMAIL.1000";
+    public static final String INVALID_EMAIL_ID = "ERROR.EMAIL.1001";
+    public static final String INVALID_MOBILE_NUMBER = "ERROR.MOBILE.1000";
+    public static final String REGISTRATION_SUCCESS = "ACK.SUCCESS.1000";
+
+}
